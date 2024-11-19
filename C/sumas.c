ed5bb64244c4805e998e3f394d71b5c0dccd0d72
IcejellyUdla <sebastian.minda@udla.edu.ec>
2024-11-19 08:23:53 -0500

    incio

--- C/sumas.c
+++ C/sumas.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-float n1, n2, resultado;
-
-
+float n1, n2, resultado=0;
+printf("ingrese n1: ");
+scanf("%f", &n1);
+printf("ingrese n2: ");
+scanf("%f", &n2);
+for (int i = 0; i < n2; i++)
+{
+    resultado= resultado+n1;
+}
+    printf("%.2f\n", resultado);
     return 0;
 }

f0449fc62866c390adb2def8dc2392407333b0f3
IcejellyUdla <sebastian.minda@udla.edu.ec>
2024-11-19 08:13:21 -0500

    inicio

--- C/Switch.c
+++ C/Switch.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+    int opc;
+    printf("sellecione una opcion:\n");
+    printf("1. serie de fibonacci\n");
+    printf("2. multiplicacion (sumas sucesivas)\n");
+    printf("3. potencia (multipplicaciones sucesivas)\n");
+    printf(">>");
+    scanf("%d",&opc);
+    switch (opc)
+    {
+    case 1:
+        printf("fibonacci\n");
+        break;
+    case 2:
+        printf("Multiplicacion\n");
+        break;
+    case 3: 
+        printf("potencia\n");
+        break; 
+    default:
+        printf("no existe la opcion");
+        break;
+    }
     return 0;
 }

5425c49e131bb7095151693e2792587a043c1b71
IcejellyUdla <sebastian.minda@udla.edu.ec>
2024-11-25 23:07:33 -0500

    git push

--- C/posibles codigos prueba/billete.c
+++ C/posibles codigos prueba/billete.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-    
+    float distancia, preciokm=0.19;
+    printf("ingrese la distancia entre las dos estaciones\n");
+    printf(">>> ");
+    scanf("%f", distancia);
+    distancia=distancia*2;
+    printf("ingrese la distancia entre las dos estaciones\n");
+    printf(">>> ");
+    scanf("%f", distancia);
     return 0;
 }

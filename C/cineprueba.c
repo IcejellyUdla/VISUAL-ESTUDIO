e8419b4418307eb25cdefa16b86df589ac0ae221
IcejellyUdla <sebastian.minda@udla.edu.ec>
2024-11-27 11:44:09 -0500

    git push

---
+++ C/cineprueba.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    float cantidadpersonas,edadpersona=0,contadorpersonas=0;
+    float precioni=3.50, precioad=7, precio3era=3, total;
+    printf("ingrese cuantas personas van a ingresar: ");
+    scanf("%f", &cantidadpersonas);
+while (contadorpersonas<cantidadpersonas)
+{
+    printf("INGRESE LA EDAD DE LA PERSONA: ");
+    scanf("%f", &edadpersona);
+   if (edadpersona>=1&&edadpersona<12)
+    {
+        edadpersona=precioni;
+        printf("La precio de la entrada de la persona 1 es de: ");
+        printf("%.2f\n", edadpersona);
+        total=total+edadpersona;
+        contadorpersonas=contadorpersonas+1;
+    }
+    else if (edadpersona>=12&&edadpersona<65)
+    {
+        edadpersona=precioad;
+        printf("La precio de la entrada de la persona 1 es de: $");
+        printf("%.2f\n", edadpersona);
+        total=total+edadpersona;
+        contadorpersonas=contadorpersonas+1;
+    }
+    else if (edadpersona>=65)
+    {
+        edadpersona=precio3era;
+        printf("La precio de la entrada de la persona es de: $");
+        printf("%.2f\n", edadpersona);
+        total=total+edadpersona;
+        contadorpersonas=contadorpersonas+1;
+    }
+    else
+    {
+        printf("EDAD INCORRECTA INGRESE DE NUEVO");
+        return 0;
+    }
+
+}
+
+    printf("El valor total por las dos entradas es de: $");
+    printf("%.2f", total);
+}

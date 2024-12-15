592e960d158d3d7daab5f201c58ab64db819e426
IcejellyUdla <sebastian.minda@udla.edu.ec>
2024-12-14 21:13:42 -0500

    Deber acabado

--- C/codigos a partir del 9_12_2024/mohamed969.c
+++ C/codigos a partir del 9_12_2024/mohamed969.c
@@ -1,61 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char *argv[]) {
-    int edadmatusalen=969, numintento, numlimite=3,numadivina;
-    char continuar="si";
-    printf("adivide la edad de Matusalen tiene 3 intentos\n");
-    printf("pista la edad esta entre 0 a 1000\n");
-   while (continuar=="si")
-   {
-    for (int numintento = 0; numintento < numlimite; numintento++)
-    {
-       printf("Que edad piensa que tiene Matusalen\n");
-        printf(">>> ");
-        scanf("%d",&numadivina);
-        if (numadivina==edadmatusalen)
+    int edadmatusalen = 969, numintento, numlimite = 3, numadivina,contalimite=3;
+    char continuar[3] = "si"; 
+
+    printf("Adivina la edad de Matusalen. Tienes 3 intentos\n");
+    printf("Pista: la edad esta entre 0 y 1000.\n");
+
+    while (strcmp(continuar, "si") == 0) {
+        for (numintento = 0; numintento < numlimite; numintento++) {
+            printf("Que edad crees que tiene Matusalen?\n>>> ");
+            scanf("%d", &numadivina);
+
+            if (numadivina == edadmatusalen) {
+                printf("Ganaste. Felicidades.\n");
+                return 0;
+            } else if (numadivina < 0 || numadivina > 1000) {
+                printf("Recuerda que la edad esta entre 0 y 1000.\n");
+                numintento=numintento-1;
+            } 
+            else if (numadivina < edadmatusalen) 
+            {
+                printf("El numero es mayor. Intenta de nuevo.\n");
+                if (numintento<numlimite-1)
+                {
+                printf("Tienes %d intentos restanes.\n", contalimite-1);
+                contalimite=contalimite-1;
+                }
+            } 
+            else 
+            {
+                printf("El numero es menor. Intenta de nuevo.\n");
+                if (numintento<numlimite-1)
+                {
+                printf("Tienes %d intentos restanes.\n", contalimite-1);
+                contalimite=contalimite-1;
+                }
+            }
+        }
+        printf("Se te acabaron los intentos. Escribe \"si\" para continuar o \"no\" para salir: ");
+        scanf("%s", continuar); 
+        if (strcmp(continuar, "no") != 0) {
+            contalimite=3;
+        }
+        else
         {
-            printf("ganaste felicidades");
+            printf("Adios.\n");
             return 0;
-        }
-        else if (numadivina<0||numadivina>1000)
-        {
-            printf("recuerda que es del 0 al 1000 la edad");
-            numintento=0;
-        }
-        else if (numadivina==0&&numadivina<=900)
-        {
-            printf("el numero es mucho mas mayor\n");
-            printf("intenta de nuevo\n");
-            numintento=numintento+1;
-        }
-        else if (numadivina>900&&numadivina<=960)
-        {
-            printf("el numero es un poco mas mayor");
-            printf("intenta de nuevo\n");
-            numintento=numintento+1;
-        }
-        else if (numadivina>960&&numadivina<=968)
-        {
-            printf("estas cerca solo un poquito mas");
-        }
-        else if (numadivina>970&&numadivina<=1000)
-        {
-            printf("te pasaste retrocede un poco");
-        }
+        }     
     }
-   printf("Tiene 0 intentos escriba ""si"" para continuar y ""no"" para cerrar");
-   scanf("%c",&continuar);
-   if (continuar=="si")
-   {
-    numintento=0;
-   }
-   else
-   {
-    printf("adios");    
     return 0;
-   }
-   
-   
-}
-    
 }
